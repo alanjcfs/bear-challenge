32781b695715d303560581803c151f87a93f50c7
Alan Fung-Schwarz <alan@atomsign.net>
2014-02-13 12:36:42 -0800

    Create new starter Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BearChallenge::Application.config.secret_key_base = '8d89e468b71b4a648de78885c8956dac9385ac6169c09ac3a2049f7f7b52056497eed6e46d36b5dbab3e04b61bc1cbdef0370b9a390e3619fe3de8b147ef8bb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BearChallenge::Application.config.session_store :cookie_store, key: '_bear-challenge_session'
