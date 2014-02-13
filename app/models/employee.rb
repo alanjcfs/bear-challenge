6c72586066bddfd61b7302a3bc562332281a0598
Alan Fung-Schwarz <alan@atomsign.net>
2014-02-13 13:22:52 -0800

    Add links and disable NaN
    Link to show average total price is shown on index, while hiding it is
    shown on the average_total_price page.
    
    When showing the average price, sometimes employees don't have an order,
    and an NaN is shown. Add a check to make sure orders isn't empty so that
    the result will be blank.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,6 +3,6 @@ class Employee < ActiveRecord::Base
 
   def average_total_price
     orders.reduce(0.0) { |acc, order| acc + order.total_price } /
-      orders.reduce(0) { |acc, order| acc + order.num_units }
+      orders.reduce(0) { |acc, order| acc + order.num_units } unless orders.empty?
   end
 end
